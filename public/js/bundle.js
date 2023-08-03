c7ac29358257bc54fbca19cef828300f319437bc
BigTako <libert761@gmail.com>
2023-08-03 07:38:32 +0300

    implemented bookings CRUD operations

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12447,7 +12447,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52106" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51579" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
